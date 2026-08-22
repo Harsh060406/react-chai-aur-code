abeedad004fe2e1d66a00949cc8a8cd587140095
Harsh Saini <harshsaini0604@gmail.com>
2026-08-22 13:42:25 +0530

    Remove unnecessary React imports from multiple components

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from "react";
+import {useState, useContext} from "react";
 import UserContext from "../context/UserContext";
 
 function Login() {
@@ -7,7 +7,7 @@ function Login() {
 
     const {setUser} = useContext(UserContext)
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         setUser({username, password})
     }
